ce3b625f2db6fc162032824f018fae2ecd4c1903
sayeed <msayeed012@gmail.com>
2021-01-14 15:21:51 +0530

    screen changes

--- src/main/java/com/example/demo/DemoController.java
+++ src/main/java/com/example/demo/DemoController.java
@@ -1,15 +1,26 @@
 package com.example.demo;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
 
-@RestController
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
 public class DemoController {
 
 	
-@GetMapping("/")
-public String getMassage()
-{
-	return "Welcome to world of DevOps for automation";
+@GetMapping({"/","/login"})
+public String getMassage(Model mod)
+{ 
+	
+		String massage = "Welcome to world of DevOps for automation";
+	mod.addAttribute("massage", massage);
+	
+	System.out.println("Welcome to world of DevOps for automation");
+	return "login";
 }
 }
---
+++ src/main/webapp/WEB-INF/jsp/login.jsp
@@ -0,0 +1,13 @@
+ <h1>${massage}</h1>
+  <table border="2" style="automargin-left: auto;
+  margin-right: auto;" >
+<tr>
+<td>Name </td><td><input type="text"></td>
+</tr>
+<tr>
+<td>Password</td><td><input type="password"><td></td></tr>
+<tr>
+<td><input type="button" value="Login"><td>
+</tr>
+</tr>
+</table>
