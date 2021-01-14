d3fdb0bfb3cdbeba11054b4c41191691d75c0992
sayeed <msayeed012@gmail.com>
2021-01-14 18:34:45 +0530

    removed unused import to avaid exeption

--- src/main/java/com/example/demo/DemoController.java
+++ src/main/java/com/example/demo/DemoController.java
@@ -1,14 +1,9 @@
 package com.example.demo;
 
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
-
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.ModelAndView;
-
+	
 @Controller
 public class DemoController {
 
