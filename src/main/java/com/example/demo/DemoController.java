c26701cd78f0df5ad9d934475a8d0f6dc92e1c6b
sayeed <msayeed012@gmail.com>
2021-01-15 08:54:01 +0530

    improved

--- src/main/java/com/example/demo/DemoController.java
+++ src/main/java/com/example/demo/DemoController.java
@@ -1,21 +1,31 @@
 package com.example.demo;
 
+import java.io.IOException;
+
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.view.RedirectView;
 	
 @Controller
 public class DemoController {
 
 	
 @GetMapping({"/","/login"})
-public String getMassage(Model mod)
+public ModelAndView  getMassage(Model mod, HttpServletResponse response)
 { 
 	
 		String massage = "Welcome to world of DevOps for automation";
 	mod.addAttribute("massage", massage);
 	
 	System.out.println("Welcome to world of DevOps for automation");
-	return "login";
+	//response.sendRedirect("login.jsp");
+	//return "login";
+	//return new RedirectView("/login.jsp");
+	return new ModelAndView("login");
 }
 }
